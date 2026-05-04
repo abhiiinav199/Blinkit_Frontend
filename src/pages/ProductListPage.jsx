4eb605478602a7c84c47ccd7780912fd317aea46
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-05-05 00:29:47 +0530

    wip

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const ProductListPage = () => {
+  const params = useParams()
+  
   return (
-    <div>ProductListPage</div>
+    <section className="min-h-[78vh] mt-3 sm:mt-0 sticky top-26 lg:top-20">
+      <div className='container sticky top-26 mx-auto grid grid-cols-[80px_1fr] md:grid-cols-[200px_1fr] lg:grid-cols-[280px_1fr]'>
+        {/* Sub Category */}
+        <div className=' bg-green-400 min-h-[78vh] '>
+        sub category
+        </div>
+
+        {/* Product */}
+        <div className=' bg-amber-300'>
+        product
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
