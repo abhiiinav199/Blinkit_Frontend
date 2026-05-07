adcac6304485cf5eb460aa3f56afc909b9080c91
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-05-07 21:30:05 +0530

    wip

--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -5,6 +5,7 @@ import AxiosToastError from '../utils/AxiosToastError'
 import axios from "../utils/axios"
 import { SummaryApi } from '../common/SummaryApi'
 import { useEffect } from 'react'
+import Loading from '../components/Loading'
 
 const ProductListPage = () => {
   const [data, setData] = useState([])
@@ -62,9 +63,29 @@ const ProductListPage = () => {
     <section className="min-h-[78vh] mt-3 sm:mt-0 sticky top-26 lg:top-20">
       <div className='container sticky top-26 mx-auto grid grid-cols-[80px_1fr] md:grid-cols-[200px_1fr] lg:grid-cols-[280px_1fr]'>
         {/* Sub Category */}
-        <div className=' bg-green-400 min-h-[78vh] '>
-          sub category
-        </div>
+        {/* <div className=' min-h-[78vh] max-h-[88vh] overflow-y-scroll  grid gap-1 shadow-md scrollbarCustom bg-white py-2'>
+          {
+            DisplaySubCatory.map((s, index) => {
+               const link = `/${valideURLConvert(s?.category[0]?.name)}-${s?.category[0]?._id}/${valideURLConvert(s.name)}-${s._id}`
+              return (
+                <Link to={link} className={`w-full p-2 lg:flex items-center lg:w-full lg:h-16 box-border lg:gap-4 border-b 
+                  hover:bg-green-100 cursor-pointer
+                  ${subCategoryId === s._id ? "bg-green-100" : ""}
+                `}
+                >
+                  <div className='w-fit max-w-28 mx-auto lg:mx-0 bg-white rounded  box-border' >
+                    <img
+                      src={s.image}
+                      alt='subCategory'
+                      className=' w-14 lg:h-14 lg:w-12 h-full object-scale-down'
+                    />
+                  </div>
+                  <p className='-mt-6 lg:mt-0 text-xs text-center lg:text-left lg:text-base'>{s.name}</p>
+                </Link>
+              )
+            })
+          }
+        </div> */}
 
         {/* Product */}
         <div className=' '>
@@ -72,6 +93,29 @@ const ProductListPage = () => {
             <h3>{subCategoryName}</h3>
           </div>
         </div>
+
+
+        {/* <div className='min-h-[80vh] max-h-[80vh] overflow-y-auto relative'>
+            <div className=' grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 gap-4 '>
+                {
+                  data.map((p, index) => {
+                    return (
+                      <CardProduct
+                        data={p}
+                        key={p._id + "productSubCategory" + index}
+                      />
+                    )
+                  })
+                }
+              </div>
+           </div> */}
+
+           {
+              loading && (
+                <Loading />
+              )
+            }
+
 
       </div>
 
