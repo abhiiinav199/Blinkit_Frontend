59d9ec29b42d46f8a4dd8f52e65967da1da3ba1b
abhiiinav199 <meshram.abhinav1998@gmail.com>
2026-05-24 18:02:17 +0530

    added cartSlice to redux 'store.jsx'

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import useReducer from "../Slice/userSlice"
 import productReducer from "../Slice/productSlice"
+import cartReducer from "../Slice/cartSlice"
 export const store = configureStore({
   reducer: {
     user : useReducer,
-    product : productReducer
+    product : productReducer,
+    cartItem : cartReducer,
+
   },
 })
